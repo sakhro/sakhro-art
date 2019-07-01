b10acd9324b8c93659a6d5ce108347cdeb1b57c0
Sava-Danylo Sakhro <sd.sakhro@gmail.com>
2019-07-01 19:48:03 +0200

    Refactor code

--- src/containers/Slideshow/Slideshow.tsx
+++ src/containers/Slideshow/Slideshow.tsx
@@ -1,18 +1,37 @@
 import React from "react";
 import SwipeableViews from "react-swipeable-views";
 
-import { Slide } from "@components";
+import { Img, Slide } from "@components";
+
+import { clientHeight } from "@constants/global";
+
+const imgStyle = {
+  height: clientHeight - 120,
+  width: "100%",
+};
 
 export const Slideshow = () => (
   <SwipeableViews enableMouseEvents>
     <Slide>
-      Slide 1
+      <Img
+        style={imgStyle}
+        src="https://via.placeholder.com/620x1200"
+        alt="test"
+      />
     </Slide>
     <Slide>
-      Slide 2
+      <Img
+        style={imgStyle}
+        src="https://via.placeholder.com/620x1200"
+        alt="test"
+      />
     </Slide>
     <Slide>
-      Slide 3
+      <Img
+        style={imgStyle}
+        src="https://via.placeholder.com/620x1200"
+        alt="test"
+      />
     </Slide>
   </SwipeableViews>
 );
